4e664571a46a3f0f0fcade292bee3fd17ed98dd8
Jeffrey Robinson <deathly809@gmail.com>
2016-01-19 23:29:56 -0600

    Renamed Beta Distribution

--- stats/discrete/beta.go
+++ stats/discrete/beta.go
@@ -7,8 +7,8 @@ import (
 	"github.com/deathly809/gomath/stats"
 )
 
-// Beta contains information about the distribution
-type Beta struct {
+// BetaBinom contains information about the distribution
+type BetaBinom struct {
 	alpha, beta      float64
 	bottom           float64
 	n                float64
@@ -17,33 +17,33 @@ type Beta struct {
 }
 
 // Pdf computes the probability density function
-func (b *Beta) Pdf(x float64) float64 {
+func (b *BetaBinom) Pdf(x float64) float64 {
 	top := stats.Beta(b.alpha+x, b.n-x+b.beta)
 	return prob.NChooseK(b.n, x) * top / b.bottom
 }
 
 // Cdf computes the cumulative density function
-func (b *Beta) Cdf(flips float64) float64 {
+func (b *BetaBinom) Cdf(flips float64) float64 {
 	return 0.0
 }
 
 // Mean is the most commong occuring result
-func (b *Beta) Mean() float64 {
+func (b *BetaBinom) Mean() float64 {
 	return b.mean
 }
 
 // Median is the middle element
-func (b *Beta) Median() float64 {
+func (b *BetaBinom) Median() float64 {
 	return b.median
 }
 
 // Variance is the skew of the data
-func (b *Beta) Variance() float64 {
+func (b *BetaBinom) Variance() float64 {
 	return b.variance
 }
 
 // StdDev is the standard deviation
-func (b *Beta) StdDev() float64 {
+func (b *BetaBinom) StdDev() float64 {
 	return b.stddev
 }
 
@@ -53,7 +53,7 @@ func NewBeta(n int, alpha, beta float64) stats.Distribution {
 
 	variance := (float64(n) * alpha * beta * (aPlusB + float64(n))) / (math.Pow(aPlusB, 2) * (aPlusB + 1))
 
-	return &Beta{
+	return &BetaBinom{
 		n:        float64(n),
 		alpha:    alpha,
 		beta:     beta,
