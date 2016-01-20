e5a45a86b3faf043c70c0d58f0301db225e35be9
Cynthia Villa <Cynthia Villa>
2016-01-19 23:15:16 -0700

    test

--- stats/functions.go
+++ stats/functions.go
@@ -25,7 +25,7 @@ func GeneralHyperGeom(p, q int, z float64, params ...float64) float64 {
 	top := 1.0
 	bottom := 1.0
 	pow := 1.0
-	for j := 1; j < 1000; j++ {
+	for j := 1; j < 100; j++ {
 		result += (top * pow) / (bottom * fact)
 
 		for i := 0; i < p; i++ {
